52efa530884bbc5452c0b82c74bf90b87d7c8ac4
Guillermo Lazo <glazovg@gmail.com>
2022-02-07 21:48:25 -0600

    Adding Employee tests

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee class', () => {
+    const employee = new Employee('Emmet','001','traveltime@btf.com');
+
+    it('Creates a new Employee', () => {
+        expect(employee).toBeInstanceOf(Employee);
+    });
+
+    describe('getName()', () => {
+        it('Returns name', () => {
+            expect(employee.getName()).toBe(employee.name);
+        });
+    });
+
+    describe('getId()', () => {
+        it('Returns id', () => {
+            expect(employee.getId()).toBe(employee.id);
+        });
+    });
+
+    describe('getEmail()', () => {
+        it('Returns email', () => {
+            expect(employee.getName()).toBe(employee.email);
+        });
+    });
+
+    describe('getRole()', () => {
+        it('Returns "Employee"', () => {
+            expect(employee.getRole()).toBe('Employee');
+        });
+    });
+});
